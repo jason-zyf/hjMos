c862082287c92d77377aa9be35ccf9f8abbb947f
zhouyunting <jason@163.com>
2020-02-20 11:53:25 +0800

    将直接从配置文件中读取的注释掉，改成读取配置对象中的属性， 发送消息增加一种方式

--- hjMos-mqTest/src/main/java/com/pci/hjmos/mq/service/impl/ProduceMsgServiceImpl.java
+++ hjMos-mqTest/src/main/java/com/pci/hjmos/mq/service/impl/ProduceMsgServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pci.hjmos.mq.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.pci.hjmos.mq.service.ProduceMsgService;
 import com.pci.hjmos.mq.util.constant.CodeConstant;
 import com.pci.hjmos.mq.util.constant.MsgConstant;
@@ -8,6 +9,8 @@ import com.pci.hjmos.mq.util.entity.ResultData;
 import com.pci.hjmos.mq.util.properties.RocketMQProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
@@ -19,6 +22,8 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.Future;
 
 @Slf4j
 @Service
@@ -35,13 +40,11 @@ public class ProduceMsgServiceImpl implements ProduceMsgService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    // kafka 的配置
-    @Value("${spring.skafka.bootstrap-servers}")
-    private String bootstrapServers;
-
+    // 直接从配置文件中获取
 //    @Value("${rocketmq.namesrvAddr}")
 //    private String namesrvAddr;
 
+    // 先放在配置类中，从配置类中获取
     @Autowired
     private RocketMQProperties rocketMqProperties;
 
@@ -50,23 +53,26 @@ public class ProduceMsgServiceImpl implements ProduceMsgService {
 
         ResultData data = new ResultData();
         try {
-
             String namesrvAddr = rocketMqProperties.getNamesrvAddr();
-            System.out.println("rocketmq的namesrv地址：" + namesrvAddr);
             if(!StringUtils.isEmpty(namesrvAddr)){
                 Message msg = new Message(topic, topic, content.getBytes());
                 SendResult sendResult = rocketMqProducer.send(msg);
                 log.info("发送一条消息到rocketmq");
                 data = new ResultData(topic, content,sendResult.getOffsetMsgId());
             }else {
-                /*ProducerRecord<String, String> record = new ProducerRecord<>(topic, content);
+                // 第一种方式，使用 kafkaProducer发送消息
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, content);
                 RecordMetadata recordMetadata = kafkaProducer.send(record).get();
-                data = new ResultData(topic, content,"JK454");*/
-                ListenableFuture<org.springframework.kafka.support.SendResult<String, String>> send =
-                        kafkaTemplate.send(topic, content);
-                System.out.println("发送一条消息到kafka，主题："+topic+",内容："+content+
-                        ",回显："+send.get().getProducerRecord().toString());
-                data = new ResultData(topic, content,send.get().getProducerRecord().toString());
+//                System.out.println("offset:"+recordMetadata.offset());    // 16
+//                System.out.println("hasOffset:"+recordMetadata.hasOffset());  // true
+                data = new ResultData(topic, content,String.valueOf(recordMetadata.offset()));
+
+                // 第一种方式，使用 kafkaTemplate 发送消息
+//                ListenableFuture<org.springframework.kafka.support.SendResult<String, String>> send =
+//                        kafkaTemplate.send(topic, content);
+//                System.out.println("发送一条消息到kafka，主题："+topic+",内容："+content+
+//                        ",回显："+send.get().getProducerRecord().toString());
+//                data = new ResultData(topic, content,send.get().getProducerRecord().toString());
             }
             return new Result(CodeConstant.RETCODE_200, MsgConstant.SUCCESS,data);
         }catch (Exception e){
