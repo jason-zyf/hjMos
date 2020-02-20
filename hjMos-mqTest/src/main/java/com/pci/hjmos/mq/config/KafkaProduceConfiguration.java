f156165586868d7179e71858d50ea397ae6c5b6b
zhouyunting <jason@163.com>
2020-02-20 11:50:57 +0800

    新建kafka生产者配置文件

---
+++ hjMos-mqTest/src/main/java/com/pci/hjmos/mq/config/KafkaProduceConfiguration.java
@@ -0,0 +1,60 @@
+package com.pci.hjmos.mq.config;
+
+import com.pci.hjmos.mq.util.properties.KafkaProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class KafkaProduceConfiguration {
+
+    // 使用对象的方式
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
+    private Map<String, Object> senderProps() {
+        Map<String, Object> props = new HashMap<>();
+
+        String innerServers = kafkaProperties.getBootstrapServers();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, innerServers);
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.ACKS_CONFIG, "1");    // 必须设置为字符串格式
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(senderProps());
+    }
+
+    @Bean //kafka发送消息模板
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<String, String>(producerFactory());
+    }
+
+    @Bean //kafka生产者
+    public Producer<String, String> producer() {
+
+        if(kafkaProperties.getBootstrapServers() == null){
+            // 说明没有配置kafka信息
+            Map<String, Object> prop = null;
+            return new KafkaProducer<String, String>(prop);
+        }
+
+        return new KafkaProducer<String, String>(senderProps());
+    }
+
+}
